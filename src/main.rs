3ca017d18bb395e182caaac836dd9c8ab2de698b
mgortman <mgortman@microsoft.com>
2017-03-30 03:16:47 -0700

    tokio-rustls on 9443

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ extern crate num_cpus;
 #[macro_use] extern crate mime;
 #[macro_use] extern crate serde_derive;
 extern crate serde_json;
+extern crate rustls;
+extern crate tokio_rustls;
 
 use tokio_proto::TcpServer;
 use futures::{future, Future, Stream};
@@ -18,7 +20,11 @@ use hyper::header::{ContentLength, ContentType};
 use hyper::status::StatusCode::NotFound;
 use std::net::SocketAddr;
 use native_tls::{TlsAcceptor, Pkcs12};
-use std::io::Read;
+use std::io::{Read, BufReader};
+use std::sync::Arc;
+use std::fs::File;
+use rustls::{ Certificate, PrivateKey, ServerConfig };
+use rustls::internal::pemfile::{ certs, rsa_private_keys };
 
 static INDEX: &'static [u8] = b"Hello, world!";
 
@@ -78,6 +84,10 @@ fn main() {
 }
 
 fn run() -> std::result::Result<(), std::io::Error> {
+    println!("Go!");
+    let s = "abc".to_string();
+    println!("{}", s);
+
     let addr: SocketAddr = "0.0.0.0:8080".parse().unwrap();
     let http_thread = std::thread::spawn(move || {
         let mut tcp = TcpServer::new(Http::new(), addr);
@@ -99,7 +109,33 @@ fn run() -> std::result::Result<(), std::io::Error> {
         tcp.serve(move || Ok(HttpServer));
     });
 
+    let mut config = ServerConfig::new();
+    config.set_single_cert(load_certs("C:/work/nayato/ht/end.fullchain"), load_private_key("C:/work/nayato/ht/end.rsa"));
+    let arc_config = Arc::new(config);
+
+    let addr: SocketAddr = "0.0.0.0:9443".parse().unwrap();
+    let https_thread = std::thread::spawn(move || {
+        let tls = tokio_rustls::proto::Server::new(Http::new(), arc_config);
+        let mut tcp = TcpServer::new(tls, addr);
+        tcp.threads(num_cpus::get());
+        tcp.serve(move || Ok(HttpServer));
+    });
+
     http_thread.join().unwrap();
     https_thread.join().unwrap();
     Ok(())
+}
+
+fn load_certs(path: &str) -> Vec<Certificate> {
+    let res = certs(&mut BufReader::new(File::open(path).unwrap())).unwrap();
+    assert!(res.len() > 0);
+    res
+}
+
+fn load_private_key(filename: &str) -> rustls::PrivateKey {
+    let keyfile = File::open(filename).expect("cannot open private key file");
+    let mut reader = BufReader::new(keyfile);
+    let keys = rustls::internal::pemfile::rsa_private_keys(&mut reader).unwrap();
+    assert!(keys.len() == 1);
+    keys[0].clone()
 }
