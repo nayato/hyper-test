8060d362c756a8ef93d30a139ac96edd709050fb
Max Gortman <mgortman@microsoft.com>
2017-11-22 09:27:54 -0800

    no proto

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,7 +1,6 @@
-use futures::{future, BoxFuture, Future, Stream};
+use futures::{future, Future, Stream};
 use futures::future::Either;
-use tokio_service::Service;
-use hyper::server::{Request, Response};
+use hyper::server::{Request, Response, Service};
 use hyper::Method::{Get, Post};
 use hyper::header::{ContentLength, ContentType, Server};
 use hyper::StatusCode::NotFound;
@@ -59,41 +58,39 @@ impl Service for HttpServer {
     type Request = Request;
     type Response = Response;
     type Error = ::hyper::error::Error;
-    type Future = Either<SyncResponseFuture, BoxFuture<Self::Response, Self::Error>>;
+    type Future = SyncResponseFuture;
 
     fn call(&self, req: Request) -> Self::Future {
         match (req.method(), req.path()) {
             (&Get, "/plaintext") | (&Get, "/") => {
                 let content = self.get_content_bytes(&req);
-                Either::A(future::ok(self.complete_response(TEXT_PLAIN, content)))
+                future::ok(self.complete_response(TEXT_PLAIN, content))
             }
             (&Get, "/json") => {
                 let content = self.get_content_str(&req);
                 let rep = TestResponse { message: content };
                 let rep_body = ::serde_json::to_vec(&rep).unwrap();
-                Either::A(future::ok(
-                    self.complete_response(APPLICATION_JSON, rep_body),
-                ))
+                future::ok(self.complete_response(APPLICATION_JSON, rep_body))
             }
-            (&Post, "/echo") => Either::B(
-                req.body()
-                    .collect()
-                    .and_then(|chunk| {
-                        let mut buffer: Vec<u8> = Vec::new();
-                        for i in chunk {
-                            buffer.append(&mut i.to_vec());
-                        }
-                        Ok(buffer)
-                    })
-                    .map(|buffer| {
-                        Response::new()
-                            .with_header(ContentLength(buffer.len() as u64))
-                            .with_header(Server::new(SERVER_NAME))
-                            .with_body(buffer)
-                    })
-                    .boxed(),
-            ),
-            _ => Either::A(future::ok(Response::new().with_status(NotFound))),
+            // (&Post, "/echo") => Either::B(
+            //     req.body()
+            //         .collect()
+            //         .and_then(|chunk| {
+            //             let mut buffer: Vec<u8> = Vec::new();
+            //             for i in chunk {
+            //                 buffer.append(&mut i.to_vec());
+            //             }
+            //             Ok(buffer)
+            //         })
+            //         .map(|buffer| {
+            //             Response::new()
+            //                 .with_header(ContentLength(buffer.len() as u64))
+            //                 .with_header(Server::new(SERVER_NAME))
+            //                 .with_body(buffer)
+            //         })
+            //         .boxed(),
+            // ),
+            _ => future::ok(Response::new().with_status(NotFound)),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 #![feature(proc_macro, conservative_impl_trait, generators, vec_resize_default)]
 
+#[macro_use]
 extern crate futures_await as futures;
 extern crate hyper;
 extern crate mime;
 extern crate native_tls;
+extern crate net2;
 extern crate num_cpus;
 extern crate rustls;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
-extern crate tokio_proto;
+extern crate tokio_core;
+extern crate tokio_io;
 extern crate tokio_rustls;
-extern crate tokio_service;
 extern crate tokio_tls;
 extern crate url;
 
-use tokio_proto::TcpServer;
+use futures::prelude::*;
+use futures::future;
+use tokio_core::reactor::Handle;
+use tokio_core::net::{TcpListener, TcpStream};
 use hyper::server::Http;
+use tokio_tls::TlsAcceptorExt;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use native_tls::{Pkcs12, TlsAcceptor};
 use std::io::{BufReader, Read};
@@ -35,43 +41,58 @@ fn run() -> std::result::Result<(), std::io::Error> {
     println!("Starting...");
 
     let any_ip = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
-    let addr: SocketAddr = SocketAddr::new(any_ip, 8080);
-    let http_thread = std::thread::spawn(move || {
-        let mut tcp = TcpServer::new(Http::new(), addr);
-        tcp.threads(num_cpus::get());
-        tcp.serve(|| Ok(http_server::HttpServer));
+    let threads = num_cpus::get();
+    let addr: SocketAddr = SocketAddr::new(any_ip, 10080);
+    let http_threads = (0..threads).map(|_| {
+        std::thread::spawn(move || {
+            serve(addr, |socket| future::ok(socket))
+        })
     });
+    // let http_thread = std::thread::spawn(move || {
+    //     let mut tcp = TcpServer::new(Http::new(), addr);
+    //     tcp.threads(num_cpus::get());
+    //     tcp.serve(|| Ok(http_server::HttpServer));
+    // });
 
-    let mut file = std::fs::File::open("identity.pfx")?;
+    let mut file = std::fs::File::open("gateway.tests.com.pfx")
+        .expect("TLS cert file must be present in current dir");
     let mut pkcs12 = vec![];
-    file.read_to_end(&mut pkcs12)?;
-    let pkcs12 = Pkcs12::from_der(&pkcs12, "password").expect("");
+    file.read_to_end(&mut pkcs12)
+        .expect("could not read TLS cert file");
+    let pkcs12 = Pkcs12::from_der(&pkcs12, "password").expect("could not load TLS cert");
     let acceptor = TlsAcceptor::builder(pkcs12).unwrap().build().unwrap();
 
-    let addr: SocketAddr = SocketAddr::new(any_ip, 8443);
-    let https_thread = std::thread::spawn(move || {
-        let tls = tokio_tls::proto::Server::new(Http::new(), acceptor);
-        let mut tcp = TcpServer::new(tls, addr);
-        tcp.threads(num_cpus::get());
-        tcp.serve(|| Ok(http_server::HttpServer));
+    let addr: SocketAddr = SocketAddr::new(any_ip, 10443);
+    let https_threads = (0..threads).map(|_| {
+        let acceptor = acceptor.clone();
+        std::thread::spawn(move || {
+            serve(addr, move |socket| init_tls(socket, acceptor.clone()))
+        })
     });
 
-    let mut config = ServerConfig::new();
-    config.set_single_cert(load_certs("end.fullchain"), load_private_key("end.rsa"));
-    let arc_config = Arc::new(config);
+    // let mut config = ServerConfig::new();
+    // config.set_single_cert(load_certs("end.fullchain"), load_private_key("end.rsa"));
+    // let arc_config = Arc::new(config);
 
-    let addr: SocketAddr = SocketAddr::new(any_ip, 9443);
-    let rustls_thread = std::thread::spawn(move || {
-        let tls = tokio_rustls::proto::Server::new(Http::new(), arc_config);
-        let mut tcp = TcpServer::new(tls, addr);
-        tcp.threads(num_cpus::get());
-        tcp.serve(|| Ok(http_server::HttpServer));
-    });
+    // let addr: SocketAddr = SocketAddr::new(any_ip, 9443);
+    // let rustls_thread = std::thread::spawn(move || {
+    //     let tls = tokio_rustls::proto::Server::new(Http::new(), arc_config);
+    //     let mut tcp = TcpServer::new(tls, addr);
+    //     tcp.threads(num_cpus::get());
+    //     tcp.serve(|| Ok(http_server::HttpServer));
+    // });
 
-    http_thread.join().unwrap();
-    https_thread.join().unwrap();
-    rustls_thread.join().unwrap();
+    for thread in https_threads.chain(http_threads) {
+        thread.join().unwrap();
+    }
+    // rustls_thread.join().unwrap();
     Ok(())
+}
+
+#[async]
+fn init_tls(socket: TcpStream, acceptor: TlsAcceptor) -> std::io::Result<tokio_tls::TlsStream<TcpStream>> {
+    let io = await!(acceptor.accept_async(socket).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e)))?;
+    Ok(io)
 }
 
 fn load_certs(path: &str) -> Vec<Certificate> {
@@ -86,4 +107,56 @@ fn load_private_key(filename: &str) -> rustls::PrivateKey {
     let keys = rustls::internal::pemfile::rsa_private_keys(&mut reader).unwrap();
     assert_eq!(1, keys.len());
     keys[0].clone()
+}
+
+fn serve<F, Ft, Io>(addr: SocketAddr, augment_io: F)
+where
+    F: Fn(TcpStream) -> Ft,
+    Ft: Future<Item = Io, Error=std::io::Error> + 'static,
+    Io: tokio_io::AsyncRead + tokio_io::AsyncWrite + 'static,
+{
+    let mut core = tokio_core::reactor::Core::new().unwrap();
+    let handle = core.handle();
+    let listener = listener(&addr, &handle).unwrap();
+
+    let server = listener.incoming().for_each(|(socket, _addr)| {
+        let conn = augment_io(socket)
+            .and_then(|io| {
+                Http::<hyper::Chunk>::new()
+                    .serve_connection(io, http_server::HttpServer).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
+            })
+            .map(|_| ())
+            .map_err(|_e| eprintln!("{:?}", _e));
+        handle.spawn(conn);
+        Ok(())
+    });
+
+    core.run(server).unwrap();
+}
+
+fn listener(addr: &SocketAddr, handle: &Handle) -> std::io::Result<TcpListener> {
+    let listener = match *addr {
+        SocketAddr::V4(_) => try!(net2::TcpBuilder::new_v4()),
+        SocketAddr::V6(_) => try!(net2::TcpBuilder::new_v6()),
+    };
+    configure_tcp(&listener)?;
+    listener.reuse_address(true)?;
+    listener.bind(addr)?;
+    listener
+        .listen(1024)
+        .and_then(|l| TcpListener::from_listener(l, addr, handle))
+}
+
+#[cfg(unix)]
+fn configure_tcp(tcp: &net2::TcpBuilder) -> std::io::Result<()> {
+    use net2::unix::*;
+
+    try!(tcp.reuse_port(true));
+
+    Ok(())
+}
+
+#[cfg(windows)]
+fn configure_tcp(_tcp: &net2::TcpBuilder) -> std::io::Result<()> {
+    Ok(())
 }
